49e5aa6079043ea6da516043b2f931229de5ab0b
Russell Barnes <russell.barnes.1@gmail.com>
2014-02-20 22:46:45 -0800

    Accept setlocation texts from Twilio, message format set

--- bikethefttracker/bikethefttracker.go
+++ bikethefttracker/bikethefttracker.go
@@ -11,6 +11,10 @@ setlocation
 <server>/setlocation?clientid=########&x=#.#####&y=#.#####&stolen=[1/0]
 	Method that the bike uses to alert the server of a theft and update its most recent location.
 
+Message format for texting Twilio phone number:
+{"clientid":"#","x":"#.#####","y":"#.#####","stolen":"[0/1]"}
+For complete URL request format, see function twiliorequest
+
 */
 
 /*
@@ -126,19 +130,76 @@ func UpdateClient(w http.ResponseWriter, r *http.Request) {
 
 }
 
+/*
+
+Bike Theft Tracker GSM request format:
+{"clientid":#,"x":"#.#####","y":"#.#####","stolen":"[0/1]"}
+or
+{%22clientid%22:%22#%22,%22x%22:%22#.#####%22,%22y%22:%22#.#####%22,%22stolen%22:%22[0/1]%22}
+
+*/
+
 func TwilioRequest(w http.ResponseWriter, r *http.Request) {
 	
 	c := appengine.NewContext(r)
 	
 	// Retrieve the body of the message
 	
-	// Fairly flat DB structure - can be optimized in the future by using clientids as parent entities
+	message := r.FormValue("Body")
+	
+	if message != "" {
+		c.Infof("Twilio SMS: ", message)
+	} else {
+		c.Infof("message is empty")
+		
+	}
+	
+	messagedata := []byte(message)
+	
+	var jsonmap map[string]interface{}
+	
+	if err := json.Unmarshal(messagedata, &jsonmap); err != nil {
+		c.Infof("Error parsing JSON from GSM: ", messagedata)
+	}
+	
+	/*x := jsonmap["x"].(float64)
+	y := jsonmap["y"].(float64)
+	clientid := jsonmap["clientid"].(float64)
+	
+	c.Infof("x: ", x, "y: ", y, "client ID: ", clientid)
+	*/
 	
 	
-	c.Infof("Twilio response received.")
+	newlocation := &Location{
+		X: jsonmap["x"].(string), 
+		Y: jsonmap["y"].(string),
+		Clientid: jsonmap["clientid"].(string),
+		Date: time.Now(),
+	}
 	
-	/* Sample request:
-	/twiliorequest?
+	// format: datastore.NewIncompleteKey(context, "subkind", *parentKey)
+	key := datastore.NewIncompleteKey(c, "User", ParentKey(c))
+    if _, err := datastore.Put(c, key, newlocation); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+	
+	// Stolen == 1? Send text and/or push notification - "open app for more details"
+	if jsonmap["stolen"].(string) == "1" {
+		
+		// Provide your own Twilio credentials and phone numbers (twilio.com)
+		accountSid, authToken := twilioaccount.GetTwilioAccount()
+		from, to := twilioaccount.GetTwilioNumbers()
+	    twilio := gotwilio.NewTwilioClient(accountSid, authToken)
+
+	    message := "Your bicycle was just stolen - open the Bike Theft Tracker app to follow"
+	    twiresponse, twiexception, twierror := twilio.SendSMS(from, to, message, "", "", c)
+		
+		c.Infof("Twilio request finished.\nResponse: ", twiresponse, "\nException: ", twiexception, "\nError: ", twierror)
+	}
+	
+	
+	/* Sample URL request:
+	<server>/twiliorequest?
 	AccountSid=###...
 	&MessageSid=###...
 	&Body=BODY_OF_MESSAGE
@@ -158,7 +219,7 @@ func TwilioRequest(w http.ResponseWriter, r *http.Request) {
 	&From=%2B1##########
 	&FromZip=97###
 	*/
-	}
+}
 
 // Get the parent key for the particular Location entity group
 func ParentKey(c appengine.Context) *datastore.Key {
